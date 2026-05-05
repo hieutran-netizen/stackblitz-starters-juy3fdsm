159f418e8453909ba8ce7f43dc46917ce1c0b5ca
hieu.tran14310 <hieu.tran@bewao.vn>
2026-05-05 15:20:01 +0700

    update

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { appRouter } from './appRouter';
-createHTTPServer({
-  router: appRouter,
-  createContext() {
-    return {};
-  },
-  basePath: '/trpc/',
-}).listen(2022);
